170de4b0412c9e72734034a2808fb11e36a415f5
Omar <orodriguez19@cmc.edu>
2016-07-18 17:09:04 -0400

    copied index.js from hoho

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,78 @@
+
+var bcrypt = require('bcrypt');
 var express = require('express');
+var models = require('./models');
+var User = models.User;
+var Message = models.Message;
+var _ = require('underscore');
+
+module.exports = function (passport) {
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  /* Authentication routes */
 
-module.exports = router;
+  router.get('/login/failure', function(req, res) {
+    res.status(401).json({
+      success: false,
+      error: req.flash('error')[0]
+    });
+  });
+
+  router.post('/login', passport.authenticate('local', {
+    successRedirect: '/login/success',
+    failureRedirect: '/login/failure',
+    failureFlash: true
+  }));
+
+  router.post('/register', function(req, res, next) {
+    var params = _.pick(req.body, ['username', 'password']);
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(params.password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        params.password = hash;
+        models.User.create(params, function(err, user) {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              error: err.message
+            });
+          } else {
+            res.json({
+              success: true,
+              user: user
+            });
+          }
+        });
+      });
+    });
+  });
+
+  // Beyond this point the user must be logged in
+  router.use(function(req, res, next) {
+    if (!req.isAuthenticated()) {
+      res.status(401).json({
+        success: false,
+        error: 'not authenticated'
+      });
+    } else {
+      next();
+    }
+  });
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.json({
+      success: true,
+      message: 'logged out.'
+    });
+  });
+
+  router.get('/login/success', function(req, res) {
+    var user = _.pick(req.user, 'username', '_id');
+    res.json({
+      success: true,
+      user: user
+    });
+  });
+
+  return router;
+};
