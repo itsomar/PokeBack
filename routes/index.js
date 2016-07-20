961526b9f9ee66105d808077a42fed0f1973191c
stevenflin <steven.lin@stern.nyu.edu>
2016-07-20 18:50:54 -0400

    populate user

--- routes/index.js
+++ routes/index.js
@@ -96,7 +96,10 @@ var router = express.Router();
 
   router.get('/feed', function(req, res, next) {
     // Post.getRecent(function(err, posts) {
-    Post.find((err, posts) => {
+    // Post.find((err, posts) => {
+    Post.find()
+        .populate("user")
+        .exec(function(err, posts) {
       if (err) return next(err);
       res.json({
         success: true,
